5467c5076385296ae2aa3dad8f75337a1fdfec5b
Jo du Plessis <jo@joduplessis.com>
2020-05-06 13:02:22 +0200

    Cleanup

--- pages/api/accounts/[token].js
+++ pages/api/accounts/[token].js
@@ -6,6 +6,7 @@ const handler = nextConnect();
 handler
   .use(middleware)
   .get(async (req, res) => {
+    /*
     try {
       const { query: { token } } = req
       const docs = await req.db.collection('accounts').find({ token }).toArray()
@@ -14,22 +15,7 @@ handler
     } catch (e) {
       res.json({ docs: null })
     }
-  })
-  .delete(async (req, res) => {
-    const { google } = require('googleapis')
-    const oAuth2Client = new google.auth.OAuth2(
-      process.env.CLIENT_ID,
-      process.env.CLIENT_SECRET,
-      process.env.REDIRECT_URL
-    )
-
-    let doc = await req.db.collection('accounts').findOne()
-
-    oAuth2Client.revokeToken(token, function(err, body) {
-
-    })
-
-    res.json(doc ? doc : {});
+    */
   })
 
 export default handler;
