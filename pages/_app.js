d1f139dac760ebc1a616682fbf0628c9de7506b0
Jo du Plessis <jo@joduplessis.com>
2020-06-11 11:48:52 +0200

    Added live token

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { initDevKit, openAppModal } from '@tryyack/dev-kit'
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    initDevKit('47e58d79-f95e-4a22-94d1-cee9aacab8df', true)
+    initDevKit('47e58d79-f95e-4a22-94d1-cee9aacab8df', false)
   }, [])
 
   return <Component {...pageProps} />
