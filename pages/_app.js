671dd07194988bfe06f6b3d693a7257a52a17a2a
Jo du Plessis <jo@joduplessis.com>
2020-06-12 11:21:31 +0200

    Moved consts to environment file (not commited)

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import { initDevKit, openAppModal } from '@tryyack/dev-kit'
+import { APP_TOKEN } from '../environment'
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    initDevKit('47e58d79-f95e-4a22-94d1-cee9aacab8df', false)
+    initDevKit(APP_TOKEN, false)
   }, [])
 
   return <Component {...pageProps} />
