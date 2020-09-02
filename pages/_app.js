0500584d3b2964eee5876db71d9a5cec5b664b86
Jo du Plessis <jo@joduplessis.com>
2020-09-02 12:35:48 +0200

    Comments

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,8 @@ import { APP_TOKEN } from '../environment'
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
+    // true = DEV
+    // ⚠️ Remember to set this for true
     initDevKit(APP_TOKEN, false)
   }, [])
 
